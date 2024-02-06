cf7326ef5d2d5434d7ed2dbd04c5775efecea268
TheJkmega <djukurb@gmail.com>
2024-02-06 12:27:16 +0100

    stuff
    EeE

--- LEGOduds_react/src/components/Contentpage.jsx
+++ LEGOduds_react/src/components/Contentpage.jsx
@@ -2,11 +2,14 @@ import {products} from '../assets/legoduds'
 import Title from './Title'
 import ProductCard from './ProductCard'
 
-export default function Contentpage ({amount, setAmount, category}) {
+export default function Contentpage ({amount, setAmount, category, setCart, cart}) {
     return(<main>
         <Title category={category} />
         {products.map(product => 
         <ProductCard 
+        cart={cart}
+        setCart={setCart}
+        prodid={product.prodid}
         key={product.prodid} 
         category={product.category} 
         title={product.title} 
